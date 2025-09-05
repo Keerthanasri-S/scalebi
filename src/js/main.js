5e69b812484733f1fd1cd9f6aae06dbb470114de
sathishk <sathishkumar.thiyagarajan@gmail.com>
2025-09-05 19:33:42 +0530

    ECharts Integrated

--- src/js/main.js
+++ src/js/main.js
@@ -8,3 +8,27 @@ import Alert from 'bootstrap/js/dist/alert';
 
 // or, specify which plugins you need:
 import { Tooltip, Toast, Popover } from 'bootstrap';
+
+import * as echarts from 'echarts';
+
+// Create the echarts instance
+var myChart = echarts.init(document.getElementById('barChart'));
+
+// Draw the chart
+myChart.setOption({
+  title: {
+    text: 'ECharts Getting Started Example'
+  },
+  tooltip: {},
+  xAxis: {
+    data: ['shirt', 'cardigan', 'chiffon', 'pants', 'heels', 'socks']
+  },
+  yAxis: {},
+  series: [
+    {
+      name: 'sales',
+      type: 'bar',
+      data: [5, 20, 36, 10, 10, 20]
+    }
+  ]
+});
