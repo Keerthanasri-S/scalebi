5237bd8ad45fe3e1ed9a3c3a58d20db7c8d7ad52
sathishk <sathishkumar.thiyagarajan@gmail.com>
2025-09-10 09:24:05 +0530

    Process ASll HTML

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig(({ mode }) => {
   return {
     base: mode === 'production' ? '/scalebi/' : '/',  // ✅ dynamic base
+    build: {
+      rollupOptions: {
+        input: {
+          main: resolve(__dirname, 'index.html'),
+          support: resolve(__dirname, 'support.html'),
+          home: resolve(__dirname, 'home.html'),
+        },
+      },
+    },
   }
 })
